5a073b17bebcbd41119ed21520f377743d1177da
Otto Miler Matulin <otto.mm.963@gmail.com>
2024-04-15 00:18:45 +0200

    Create new list of food in app

--- RNList/App.js
+++ RNList/App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar, SafeAreaView, FlatList } from 'react-native';
+import foodList from "./foodData.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <ScrollView style={styles.scrollView}>
+      {
+        foodList.map(food => {
+          return (
+            <View style={styles.card} key={food.foodName}>
+              <Text style={styles.cardText}>{food.foodName}</Text>
+              <Text>{food.foodType}</Text>
+              <Text>{food.calories}</Text>
+            </View>
+          )
+        })
+      }
+      </ScrollView> */}
+      <View style={styles.scrollView}>
+      <FlatList 
+        data={foodList}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.card} key={item.foodName}>
+              <Text style={styles.cardText}>{item.foodName}</Text>
+              <Text>{item.foodType}</Text>
+              <Text>{item.calories}</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.foodName.toString()}
+      />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "tan",
+    marginBottom: 16,
+  },
+  cardText: {
+    fontSize: 18,
   },
 });
