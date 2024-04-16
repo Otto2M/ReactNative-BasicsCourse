dec1896d58e98bf8b1c40381aa091e3067569c09
Otto Miler Matulin <otto.mm.963@gmail.com>
2024-04-17 00:35:21 +0200

    POST request implemented

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, View, StyleSheet, FlatList, Text, ActivityIndicator } from 'react-native';
+import { SafeAreaView, StatusBar, View, StyleSheet, FlatList, Text, ActivityIndicator, TextInput, Button } from 'react-native';
 import { useState, useEffect} from "react";
 
 export default function App() {
@@ -6,6 +6,9 @@ export default function App() {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -20,6 +23,25 @@ export default function App() {
     fetchData(20)
     setRefreshing(false)
   };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody
+      })
+    });
+    const newPost = await response.json()
+    setPostList([newPost, ...postList])
+    setPostTitle("")
+    setPostBody("")
+    setIsPosting(false)
+  }
 
   useEffect(() => {
     fetchData();
@@ -37,6 +59,12 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
+      <>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='Naslov' value={postTitle} onChangeText={setPostTitle}/>
+        <TextInput style={styles.input} placeholder='Opis' value={postBody} onChangeText={setPostBody}/>
+        <Button title={isPosting ? "Dodavanje..." : "Dodaj post"} onPress={addPost} disabled={isPosting}/>
+      </View>
       <View style={styles.listContainer}>
         <FlatList 
           data= {postList}
@@ -56,6 +84,7 @@ export default function App() {
           onRefresh={handleRefresh}
         />
       </View>
+      </>
     </SafeAreaView>
   );
 }
@@ -106,4 +135,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 8,
+    padding: 8,
+  },
+
 });
