a5f69c22885121547fc1e83bef0d10ff2ae747ff
Otto Miler Matulin <otto.mm.963@gmail.com>
2024-04-17 23:48:45 +0200

    Add error handling

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -9,13 +9,22 @@ export default function App() {
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
   const [isPosting, setIsPosting] = useState(false);
+  const [error, setError] = useState("");
 
   const fetchData = async (limit = 10) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
-    const data = await response.json()
+    try {
+      const response = await fetch(`https://jsonplaceholders.typicode.com/posts?_limit=${limit}`);
+      const data = await response.json()
 
-    setPostList(data);
-    setIsLoading(false);
+      setPostList(data);
+      setIsLoading(false);
+      setError("");
+    } catch (error) {
+      console.error("Greška prilikom dohvaćanja podataka: ", error);
+      setIsLoading(false);
+      setError("Nije moguće učitati podatke!");
+    }
+    
   };
 
   const handleRefresh = () => {
@@ -26,22 +35,28 @@ export default function App() {
 
   const addPost = async () => {
     setIsPosting(true);
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        title: postTitle,
-        body: postBody
-      })
-    });
-    const newPost = await response.json()
-    setPostList([newPost, ...postList])
-    setPostTitle("")
-    setPostBody("")
-    setIsPosting(false)
-  }
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title: postTitle,
+          body: postBody
+        })
+      });
+      const newPost = await response.json()
+      setPostList([newPost, ...postList])
+      setPostTitle("")
+      setPostBody("")
+      setIsPosting(false)
+      setError("");
+    } catch (error) {
+      console.error("Pgreška prilikom dodavanja novog zapisa: ", error);
+      setError("Nije moguće dodati novi zapis!");
+    }
+  };
 
   useEffect(() => {
     fetchData();
@@ -59,6 +74,11 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ): (
       <>
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} placeholder='Naslov' value={postTitle} onChangeText={setPostTitle}/>
@@ -85,8 +105,9 @@ export default function App() {
         />
       </View>
       </>
+    )}
     </SafeAreaView>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -150,5 +171,17 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     padding: 8,
   },
-
+  errorContainer: {
+    backgroundColor: "FFC0CB",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+    alignItems: 'center',
+  },
+  errorText: {
+    color: "#D8000C",
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
