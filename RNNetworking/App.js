ab61107b3e931ca883b9942a4764fc9e23eb651c
Otto Miler Matulin <otto.mm.963@gmail.com>
2024-04-17 23:56:53 +0200

    Update error handling

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -13,7 +13,7 @@ export default function App() {
 
   const fetchData = async (limit = 10) => {
     try {
-      const response = await fetch(`https://jsonplaceholders.typicode.com/posts?_limit=${limit}`);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
       const data = await response.json()
 
       setPostList(data);
@@ -172,7 +172,7 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   errorContainer: {
-    backgroundColor: "FFC0CB",
+    backgroundColor: "#FFC0CB",
     padding: 16,
     borderRadius: 8,
     borderWidth: 1,
