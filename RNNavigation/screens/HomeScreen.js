aee23c57d1ffc69e9fcf443c36abe4a833af3a55
Otto Miler Matulin <otto.mm.963@gmail.com>
2024-04-18 14:07:39 +0200

    Navigation in RN
    Navigation and passing data between screens, stack navigation

---
+++ RNNavigation/screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function HomeScreen({navigation, route}) {
+  // const navigation = useNavigation()
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>{route.params?.result}</Text>
+      <Button title='Go to About' onPress={() => navigation.navigate("About")}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  }
+});
